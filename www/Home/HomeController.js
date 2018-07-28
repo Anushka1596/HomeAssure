67b9ae04d6651d6c955543d3ae3a3efac6b369d5
Unknown <kshama.bhumca2015@gmail.com>
2018-07-28 10:43:11 +0530

    install

--- www/Home/HomeController.js
+++ www/Home/HomeController.js
@@ -1,8 +1,16 @@
-app.controller('HomeCtrl', function ($scope) {
+app.controller('HomeCtrl', ['$scope','ionicDatePicker',function ($scope,ionicDatePicker) {
   $scope.message = "Hello world";
   $scope.fixedSettings = {
     theme: 'material-dark',
     layout: 'fixed',
     itemWidth: 80
   };
-})
+  var ipObj1 = {
+    callback: function (val) {  
+      console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+    }
+  };
+  $scope.openDatePicker = function(){
+    ionicDatePicker.openDatePicker(ipObj1);
+  };
+}]);
